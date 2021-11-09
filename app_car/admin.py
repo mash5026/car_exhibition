cb4d4a5ca0c1254935557f0cb16f749abff61368
mahdi <shamsmahdi098@gmail.com>
2021-11-09 13:48:50 +0330

    the last changes in 18 aban 1400

--- app_car/admin.py
+++ app_car/admin.py
@@ -1,15 +1,60 @@
 from django.contrib import admin
-
 from django.contrib import admin
 from .models import Car, Company,Agency
+from django.contrib import messages
+from django.db import models
+
+
 # Register your models here.
+class CarInlineCompany(admin.TabularInline):
+    model = Car
+    extra = 0
+
+
+class CarInlineAgency(admin.TabularInline):
+    model = Car
+    extra = 2
+
+
+@admin.action(description="ناموجود کردن ماشین ها")
+def make_unavailable_car(modeladmin,request,queryset):
+    unavailable = queryset.update(stock=False)
+    messages.success(request,f"{unavailable} ماشین ناموجود گردید")
+
+@admin.action(description="موجود کردن ماشین ها")
+def make_available_car(modeladmin,request,queryset):
+    available = queryset.update(stock=True)
+    messages.success(request,f"{available} ماشین به حالت موجود تغییر یافت.")
+
+@admin.action(description="افزایش قیمت به میزان 20درصد")
+def increase_price(modeladmin,request,queryset):
+    all_price = list(queryset.values_list('price'))
+    all_cars = queryset.all()
+    numbers = queryset.count()
+    new_price = [(i[0]*1.2) for i in all_price]
+    for i in range(len(new_price)):
+        my_car = all_cars[i]
+        my_car.price = new_price[i]
+        my_car.save()
+    messages.success(request, f"{numbers} قیمت به روز رسانی گردید.")
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('name','year','price')
+    list_display = ('get_image','name','year','price','stock','company','get_date')
     list_filter = ('name', 'company','agency','year','price','stock')
-    search_fields = ('name', 'company','agency','year','price')
+    search_fields = ('name','company__name','year','price')
+    actions = (make_unavailable_car,make_available_car,increase_price)
+    list_display_links = ('name', 'company')
+    list_per_page = 100
+    sortable_by = ('name', 'company')
+    list_editable = ('price','year')
+    ordering = ('-year',)
+    # readonly_fields = ('agency',)
+
+    # def get_date(self,obj):
+    #     return obj.created_at.strftime('در ساعت %H:%m و در تاریخ %Y/%m/%d')
+    # get_date.short_description = "تاریخ ایجاد"
 
 
 @admin.register(Company)
@@ -17,12 +62,15 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name','country')
     list_filter = ('name','country')
     search_fields = ('name','country')
-
+    inlines = (CarInlineCompany,)
+    
 
 @admin.register(Agency)
 class AgencyAdmin(admin.ModelAdmin):
     list_display = ('name','code','status','url_site')
     list_filter = ('name','code', 'status')
     search_fields = ('name','code')
+    inlines = (CarInlineAgency,)
+    
 
 
--- app_car/models.py
+++ app_car/models.py
@@ -1,19 +1,38 @@
 from django.db import models
 from .utils import path_car,LIST_STATUS,OFFICIAL, validat_image
+from django.utils.text import slugify
+from django_jalali.db import models as jmodels
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 
 class Car(models.Model):
+    slug = models.SlugField(verbose_name="پیوند یکتا", null=True, blank=True, allow_unicode=True)
     name = models.CharField(max_length=30, verbose_name="نام خودرو", null=True)
     company = models.ForeignKey("Company", verbose_name="نام برند", on_delete=models.SET_NULL, null=True, blank=True,\
     related_name='cars')
     agency = models.ForeignKey("Agency", verbose_name="نام نمایندگی", on_delete=models.CASCADE,\
     related_name='cars')
+    created_at = jmodels.jDateTimeField(verbose_name="تاریخ ثبت",auto_now_add=True, null=True, blank=True )
     year = models.PositiveSmallIntegerField(verbose_name="سال تولید", null=True)
     price = models.PositiveBigIntegerField(verbose_name="قیمت", null=True)
     stock = models.BooleanField(verbose_name="موجود", default=True ,help_text="در صورت موجود بودن علامت کنار آن قابل مشاهده است")
     image = models.ImageField(verbose_name="تصویر خودرو", upload_to =path_car, validators=[validat_image])
 
+    def save(self,*args,**kwrags):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args,**kwrags)
+    
+    def get_date(self):
+        return self.created_at.strftime('در ساعت %H:%m و در تاریخ %Y/%m/%d')
+    get_date.short_description="تاریخ ثبت"
+
+    def get_image(self):
+        if self.image:
+            return mark_safe(f'<img src="{self.image.url}" alt="{self.name}" width="150" height="100">')
+        else:
+            return mark_safe('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsNGGjrfSqqv8UjL18xS4YypbK-q7po_8oVQ&usqp=CAU" width="100" height="100">')
+    get_image.short_description = 'عکس خودرو'
 
     class Meta:
         verbose_name = "نام خودرو"
